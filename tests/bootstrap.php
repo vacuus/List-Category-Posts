b852cd70615814258b9dbf1719b137f3166b666d
Alejandro Angulo <aab.j13@gmail.com>
2017-01-07 10:41:38 -0800

    update test scaffolding

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,25 @@
 <?php
 /**
- * Bootstrap the testing environment.
+ * PHPUnit bootstrap file
  *
- * Note: Do note change the name of this file. PHPUnit will automatically fire this file when run.
+ * @package List_Category_Posts
  */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/var/www/wp-tests-lib';
+	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
+// Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
+/**
+ * Manually load the plugin being tested.
+ */
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/list-category-posts.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package List_Category_Posts
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
